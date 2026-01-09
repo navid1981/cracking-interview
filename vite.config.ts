507a2c6554e28a3de0c1a3b711ee13e3950987c4
navid1981 <navid.vaziri@outlook.com>
2026-01-08 23:45:10 -0600

    windows

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   clearScreen: false,
+  base: './',
   server: {
+    host: '127.0.0.1',
     port: 1420,
     strictPort: true,
   },
@@ -13,5 +15,6 @@ export default defineConfig({
     target: ['es2021', 'chrome100', 'safari13'],
     minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
     sourcemap: !!process.env.TAURI_DEBUG,
+    outDir: 'dist',
   },
 })
